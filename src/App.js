d5a9390fbceda88772698635f366e56d290b6c71
nyrellcl <nyrell.leonor2000@gmail.com>
2023-02-12 19:45:01 -0800

    styled start page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/color-generator" exact element={<StartGame/>}/>
+        <Route path="/color-generator/" exact element={<StartGame/>}/>
         <Route path="/game" element={<ColorGame/>}/>
       </Routes>
     </>
--- src/components/StartGame.js
+++ src/components/StartGame.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {QuestionSquare} from "react-bootstrap-icons"
 
 function StartGame() {
     return (
-        <section>
-            <h1>Let's Guess the Color!</h1>
-            <Link to="/game"><button type="button">Let's Play!</button></Link>
+        <section className="start-container">
+            <QuestionSquare className="question-mark"/>
+            <h1 className='start-container__title'>Let's Guess the Color!</h1>
+            <article className="start-container__content">
+                <h2>Press button to start</h2>
+                <Link to="/game"><button type="button">Let's Play!</button></Link>
+
+            </article>
         </section>
     )
 }
